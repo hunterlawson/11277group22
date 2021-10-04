f0d5033d8e04c20c9924486483aef2aa6b4c806f
Hunter Lawson <hunterlaw9son@gmail.com>
2021-10-04 18:42:44 -0400

    Add front-end directories

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'hello, world!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
