453fda014ef5a2bddffaa00da74e742b3925bb62
huangjiayu <Peter.Huang518@outlook.com>
2021-10-05 17:57:39 -0400

    home page

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def home():
-    return 'hello, world!'
+    return render_template('homePage.html', title='Home')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
