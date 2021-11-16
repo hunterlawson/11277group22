975f24d185d753f015e92e700c08fdf54cae04f3
Hunter Lawson <hunterlaw9son@gmail.com>
2021-11-16 01:07:42 -0500

    Add user authentication and login

--- sunspot/__init__.py
+++ sunspot/__init__.py
@@ -2,15 +2,20 @@ from flask import Flask
 from mongoengine import connect
 from os import environ
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
 # Set the Flask secret key, which is used for things like authenticating
 # the forms and for user authentication
 app.config['SECRET_KEY'] = environ.get('SUNSPOT_SECRET_KEY')
-bcrypt = Bcrypt(app)
 
 app.config['MONGO_URI'] = environ.get('SUNSPOT_MONGO_URI')
 connect(host=app.config['MONGO_URI'])
+
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "danger"
 
 from sunspot import views
--- sunspot/models.py
+++ sunspot/models.py
@@ -1,11 +1,23 @@
 from mongoengine import *
+from sunspot import login_manager
+from flask_login import UserMixin
+from datetime import datetime
 
+@login_manager.user_loader
+def load_user(user_id):
+    try:
+        return User.objects.get(id=user_id)
+    except Exception as e:
+        print(e)
+    
+    return None
+    
 class Bookmark(EmbeddedDocument):
-    location = StringField(required=True)
-    date = DateTimeField(required=True)
+    location = DictField(required=True)
+    date = DateTimeField(default=datetime.utcnow)
 
-class User(Document):
-    email = StringField(required=True)
+class User(Document, UserMixin):
+    email = StringField(required=True, unique=True)
     username = StringField(required=True)
     password = StringField(required=True)
     admin = BooleanField()
