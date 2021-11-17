8bf9e3fe55509774aa289f81673b96b575ecefe0
Hunter Lawson <hunterlaw9son@gmail.com>
2021-11-16 19:59:10 -0500

    add api access

--- sunspot/__init__.py
+++ sunspot/__init__.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 # Set the Flask secret key, which is used for things like authenticating
 # the forms and for user authentication
 app.config['SECRET_KEY'] = environ.get('SUNSPOT_SECRET_KEY')
+app.config['SOLAR_API_KEY'] = environ.get('SUNSPOT_SOLAR_API_KEY')
 
 app.config['MONGO_URI'] = environ.get('SUNSPOT_MONGO_URI')
 connect(host=app.config['MONGO_URI'])
